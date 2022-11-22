d5ba50a6b91484681ce00815731a04304b3d2271
Alexander Gordey <alexnickgordey3@gmail.com>
2022-11-22 12:47:56 +0300

    задачи про матрицы 2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 n = int(input())
+m = int(input())
 matrix = [0] * n
 for i in range(n):
-    matrix[i] = [] * n
+    matrix[i] = [] * m
 s = ''
 ls1 = []
 for i in range(n):
@@ -10,9 +11,13 @@ for i in range(n):
     for j in range(len(ls)):
         ls[j] = int(ls[j])
         matrix[i].append(ls[j])
+maxi = matrix[0][0]
+x = 0
+y = 0
 for i in range(n):
-    for j in range(n):
-        if i > j - 1:
-            ls1.append(matrix[i][j])
-
-print(max(ls1))
+    for j in range(m):
+        if matrix[i][j] > maxi:
+            maxi = matrix[i][j]
+            x = i
+            y = j
+print(x, y)
