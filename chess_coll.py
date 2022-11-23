99131b1d2747b59f26ec86c6c567f978af67ec56
Alexander Gordey <alexnickgordey3@gmail.com>
2022-11-23 13:34:49 +0300

    крутая задача про шахм коня

---
+++ chess_coll.py
@@ -0,0 +1,20 @@
+n = 8
+xy = input()
+y = '87654321'.index(xy[1])
+x = 'abcdefgh'.index(xy[0])
+matrix = ['.'] * n
+for i in range(n):
+    matrix[i] = ['.'] * n
+
+for i in range(n):
+    for j in range(n):
+        if x == j and y == i:
+            matrix[j][i] = 'N'
+        INX = (x - j) * (y - i)
+        if INX == 2 or INX == -2:
+            matrix[j][i] = '*'
+
+for i in range(n):
+    for j in range(n):
+        print(matrix[j][i], end=' ')
+    print()
