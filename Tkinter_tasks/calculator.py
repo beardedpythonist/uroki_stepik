f90fc679349963f3d5f72f28626c1b91a4bd14fb
Alexander Gordey <alexnickgordey3@gmail.com>
2022-12-06 12:59:24 +0300

    экзамен на множества

--- Tkinter_tasks/calculator.py
+++ Tkinter_tasks/calculator.py
@@ -1,80 +1,48 @@
 from tkinter import *
 
-root = Tk()
-e = Entry(width=20, font='arial 27')
-e1 = Entry(width=20, font='arial 27')
-b_plus = Button(text='+', font='arial 27')
-b_plus['bg'] = '#555555'
-b_minus = Button(text='-', font='arial 27')
-b_umnozh = Button(text='*', font='arial 27')
-b_delit = Button(text='/', font='arial 27')
-l1 = Label(bg='black', fg='white', width=20, font='arial 27')
-def plus(event):
-    s = e.get()
-    if s.isdigit() == False:
-        l1['text'] = str('Ошибка')
-    else:
-        s = float(s)
-        s1 = e1.get()
-    if s1.isdigit() == False:
-        l1['text'] = str('Ошибка')
-    else:
-        s1 = float(s1)
-    total = s + s1
-    l1['text'] = str(total)
-b_plus.bind('<Button-1>', plus)
-def minus(event):
-    s = e.get()
-    if s.isdigit() == False:
-        l1['text'] = str('Ошибка')
-    else:
-        s = float(s)
-        s1 = e1.get()
-    if s1.isdigit() == False:
-        l1['text'] = str('Ошибка')
-    else:
-        s1 = float(s1)
-    total = s - s1
-    l1['text'] = str(total)
-b_minus.bind('<Button-1>', minus)
-def umnozh(event):
-    s = e.get()
-    if s.isdigit() == False:
-        l1['text'] = str('Ошибка')
-    else:
-        s = float(s)
-        s1 = e1.get()
-    if s1.isdigit() == False:
-        l1['text'] = str('Ошибка')
-    else:
-        s1 = float(s1)
-    total = s * s1
-    l1['text'] = str(total)
-b_umnozh.bind('<Button-1>', umnozh)
-def deli(event):
-    s = e.get()
-    if s.isdigit() == False:
-        l1['text'] = str('Ошибка')
-    else:
-        s = float(s)
-        s1 = e1.get()
-    if s1.isdigit() == False:
-        l1['text'] = str('Ошибка')
-    else:
-        s1 = float(s1)
-    total = s / s1
-    l1['text'] = str(total)
-b_delit.bind('<Button-1>',deli)
+window = Tk()
+window.title('Чудо-машина')
+window.geometry('500x580+450+50')
+window.resizable(False, False)
+window.config(bg='#BC8F8F')
 
-e.pack()
-e1.pack()
-l1.pack()
-b_plus.pack(side=LEFT)
-b_minus.pack(side=LEFT)
-b_umnozh.pack(side=LEFT)
-b_delit.pack(side=LEFT)
-
-root.mainloop()
+# создание окна вывода результата
+formula = 0
+lbl = Label(text=formula, font=('Roboto', 30, 'bold'), bg='#BC8F8F')
+lbl.place(x=11, y=50)
 
 
+def calc(y):
+    global formula
+    if y == 'C':
+        formula = ''
+    elif y == 'del':
+        formula = formula[0:-1]
+    elif y == 'x ^ 2':
+        formula = str(eval(formula) ** 2)
+    elif y == '=':
+        formula = str(eval(formula))
+    elif y == '+/-':
+        formula = str(eval(formula) * -1)
+    else:
+        if formula == 0:
+            formula = ''
+        formula += y
+    lbl.configure(text=formula)
+
+
+# создаем кнопки
+buttons = ['C', 'del', '+', '=', '1', '2', '3', '/', '4', '5', '6', '+', '7', '8', '9', '-', '+/-', '0', '%', 'x ^ 2']
+x = 18
+y = 140
+
+# разбмещаем и подписываем кнопки  и связываем с фунцией
+for button in buttons:
+    get_lbl = lambda x=button: calc(x)
+    Button(text=button, bg='#EEE8AA', font=('Roboto, 20'), command=get_lbl).place(x=x, y=y, width=115, height=79)
+    x += 117
+    if x > 400:
+        x = 18
+        y += 81
+window.mainloop()
 
